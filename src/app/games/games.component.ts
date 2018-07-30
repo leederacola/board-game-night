bf632b65409df9e855caa8d07f8dd0ce615d87eb
Ryan <Ryan@CROCODILE>
2018-07-30 14:28:34 -0400

    games list comming from firebase database!

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 import { Game } from 'src/app/models/game';
 import { GameService } from 'src/app/game.service';
 import { DataFriendService } from '../data-friend.service';
+import { FiregameService } from '../firegame.service';
 
 @Component({
   selector: 'app-games',
@@ -18,12 +20,42 @@ export class GamesComponent implements OnInit {
   //selectedGame: Game;
   playerCount: number;
   listToggle: boolean = false;
+
+  fireGamesList: Game[] = [];
+
+    // game service is injected into the dashboard component and can be used
+    constructor(
+      private gameService: GameService,
+      private fireGameService: FiregameService,
+      private dataService: DataFriendService) { }
+  
+    ngOnInit() {
+      this.getGamesList();
+      // this.gameService.getGames()
+      //   .subscribe(games => this.games = games);  
+
+      this.dataService.currentPlayerCount.subscribe(ct => this.playerCount = ct);
+    }
+    
+
+
   // used gameService to set component games[]
   getGames(): void {
-    this.gameService.getGames()
+    //this.gameService.getGames().subscribe(games => this.games = games);
       // names result of subscrition games and assigns to this.games property
-      .subscribe(games => this.games = games);
+      
   }
+  getGamesList() {
+    // Use snapshotChanges().map() to store the key
+    this.fireGameService.getGamesList().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    ).subscribe(games => {
+      this.fireGamesList = games;
+    });
+  }
+ 
 
   toggleGameList(){
     this.listToggle = !this.listToggle;
@@ -32,16 +64,6 @@ export class GamesComponent implements OnInit {
 
 
 
-  // game service is injected into the dashboard component and can be used
-  constructor(
-    private gameService: GameService,
-    private dataService: DataFriendService) { }
 
-  ngOnInit() {
-    this.gameService.getGames()
-      .subscribe(games => this.games = games);
-
-    this.dataService.currentPlayerCount.subscribe(ct => this.playerCount = ct);
-  }
 
 }
